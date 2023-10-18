f885e5346fc6d278b7e82beea47a0750d5954e95
Junyoung Cho <cjydoo@gmail.com>
2023-10-18 15:47:43 +0900

    draw uniform B-spline curve

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'https://threejs.org/build/three.module.js';
 
 let scene, camera, renderer;
+let controlPoints = [];
+let curveObject;
 
 init();
 animate();
@@ -20,7 +22,7 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    // 마우스 클릭 이벤트 리스너 추가
+    // 마우스 클릭 이벤트 리스너 추가 
     window.addEventListener('click', onDocumentMouseDown, false);
 }
 
@@ -39,6 +41,53 @@ function onDocumentMouseDown(event) {
 
     // 점을 씬에 추가
     scene.add(circle);
+
+    controlPoints.push(circle.position);
+
+    if (controlPoints.length >= 3) {
+        const curvePoints = [];
+        for (let u = 0; u <= controlPoints.length; u += 0.01) {
+            curvePoints.push(computeBSpline(controlPoints, u));
+        }
+
+        if (curveObject) {
+            scene.remove(curveObject); // 기존 곡선 제거
+        }
+
+        const curveGeometry = new THREE.BufferGeometry().setFromPoints(curvePoints);
+        const curveMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+        curveObject = new THREE.Line(curveGeometry, curveMaterial);
+        scene.add(curveObject);
+    }
+}
+function computeBSpline(controlPoints, u) {
+    const n = controlPoints.length - 1;
+    let point = new THREE.Vector2();
+
+    const knots = [];
+
+    // Create a uniform knot vector with duplicates at the beginning and end
+    for (let i = 0; i <= n + 3; i++) {
+        knots.push(i);
+    }
+
+    for (let i = 0; i <= n; i++) {
+        const Ni = N(i, 2, u, knots);
+        point.x += controlPoints[i].x * Ni;
+        point.y += controlPoints[i].y * Ni;
+    }
+    
+    return point;
+}
+
+function N(i, k, u, knots) {
+    if (k === 0) {
+        return (u >= knots[i] && u < knots[i + 1]) ? 1 : 0;
+    }
+    const a = (u - knots[i]) / (knots[i + k] - knots[i]) * (knots[i + k] !== knots[i] ? N(i, k - 1, u, knots) : 0);
+    const b = (knots[i + k + 1] - u) / (knots[i + k + 1] - knots[i + 1]) * (knots[i + k + 1] !== knots[i + 1] ? N(i + 1, k - 1, u, knots) : 0);
+
+    return a + b;
 }
 
 function animate() {
